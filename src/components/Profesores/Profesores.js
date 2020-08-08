1f7654d5ce31d4cf6c317aed3181fd79d5df88a5
= <bigbigbyned@gmail.com>
2020-08-08 19:57:54 -0400

    Checkpoint

--- src/components/Profesores/Profesores.js
+++ src/components/Profesores/Profesores.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 const Profesores = () => {
-    return(
-        <div>
-            Hola desde Profesores
+    return (
+        <div className="content-container">
+            <h1 className="content-title">Tesistas</h1>
+            <div className="form-container">
+                <ProfesoresForm />
+            </div>
+            <div className="table-container">
+                <ProfesoresTable />
+            </div>
         </div>
     )
 }
