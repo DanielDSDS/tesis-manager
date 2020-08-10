3da41f1fe673d397960a05f8d0422785770258c9
= <bigbigbyned@gmail.com>
2020-08-10 10:17:52 -0400

    Daniel - Routing to individual propuesta

--- src/components/Propuestas/Propuesta.js
+++ src/components/Propuestas/Propuesta.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react'
 
 const Propuesta = ({ location }) => {
-    console.log(location.state)
+    const { fec_entrega, id_propuesta, nombre_t, titulo_propuesta } = location.state
+
     return (
-        <div>
-            Hola desde una propuesta especifica
+        <div className="content-container">
+            <h2 className="content-title">Propuesta #{id_propuesta}</h2>
+            <h4 className="content-subtitle">{titulo_propuesta}</h4>
+            <div className="propuesta-container">
+            </div>
         </div>
     )
 }
